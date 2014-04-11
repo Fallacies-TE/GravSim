426ba0505887db54f3cafca6dc790e343635c06b
David Argueta <divs2002@gmail.com>
2014-04-11 12:40:57 -0400

    Added Git Ignore file that excludes certain Eclipse files and C++ files. Added empty SolarSystem file with header

---
+++ GravitySimulator/SolarSystem.cpp
@@ -0,0 +1,18 @@
+/*
+ * SolarSystem.cpp
+ *
+ *  Created on: Apr 10, 2014
+ *      Author: davidjanette
+ */
+
+#include "SolarSystem.h"
+
+SolarSystem::SolarSystem() {
+	// TODO Auto-generated constructor stub
+
+}
+
+SolarSystem::~SolarSystem() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ GravitySimulator/SolarSystem.h
@@ -0,0 +1,17 @@
+/*
+ * SolarSystem.h
+ *
+ *  Created on: Apr 10, 2014
+ *      Author: davidjanette
+ */
+
+#ifndef SOLARSYSTEM_H_
+#define SOLARSYSTEM_H_
+
+class SolarSystem {
+public:
+	SolarSystem();
+	virtual ~SolarSystem();
+};
+
+#endif /* SOLARSYSTEM_H_ */

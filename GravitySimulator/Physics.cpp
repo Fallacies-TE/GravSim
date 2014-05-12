c84081a55166a3777b60838cff5862fb373afc4b
sdashner <sdashner@sdashner-VirtualBox.(none)>
2014-05-11 20:18:23 -0400

    committing to pull

--- GravitySimulator/Physics.cpp
+++ GravitySimulator/Physics.cpp
@@ -34,16 +34,58 @@ void Physics::step(){
         p->_z += (p->_z_speed+cp->_z_speed)*interval/2;
     }
     currentStep = nextStep;
+} 
+/*
+void Physics::step(){
+    for (int i = 0; i < currentStep.size(); i++){
+        SpaceObject *p = &currentStep[i];
+        bool not_crashing = true;
+        for (int j = 0; j < currentStep.size(); j++) {
+            if (j == i || j >= 10000) {
+                break;
+            }
+
+            const SpaceObject *p1 = &currentStep[j];
+
+            double distance = sqrt(pow(p1->_x-p->_x,2)+pow(p1->_y-p->_y,2)+pow(p1->_z-p->_z,2));
+            if (distance > p1->_radius){
+                p->_x_speed += G * p1->_mass / (distance * distance) * (p1->_x - p->_x)/distance;
+                p->_y_speed += G * p1->_mass / (distance * distance) * (p1->_y - p->_y)/distance;
+                p->_z_speed += G * p1->_mass / (distance * distance) * (p1->_z - p->_z)/distance;
+
+            } else {
+                // something happens when they crash
+                not_crashing = false;
+            }
+
+            if (not_crashing){
+                p->_x += p->_x_speed;
+                p->_y += p->_y_speed;
+                p->_z += p->_z_speed;
+
+            }
+            else {
+                // delete the object somehow
+            }
+
+        }
+    }
 }
+*/
 
 void Physics::calc(SpaceObject *p1,SpaceObject *p2,SpaceObject *np1,SpaceObject *np2){
+    // square root of x1 - x2 squared plus y1 - y2 squared plus z1 - z2 squared
+    // gives us the three dimensional distance between the two objects
     double distance = sqrt(pow(p1->_x-p2->_x,2)+pow(p1->_y-p2->_y,2)+pow(p1->_z-p2->_z,2));
+    
     double radX = (p1->_x-p2->_x)/distance;
     double radY = (p1->_y-p2->_y)/distance;
     double radZ = (p1->_z-p2->_z)/distance;
-
+    
     double g = G/pow(distance,2);
+
     double acc1 = g*p2->_mass;
+
     np1->_x_speed -= acc1*radX*interval;
     np1->_y_speed -= acc1*radY*interval;
     np1->_z_speed -= acc1*radZ*interval;
